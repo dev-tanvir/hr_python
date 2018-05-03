8d0753a82bb70c9b3518ebf85c3c1fc7d71a2832
tanvir-faisal <tanvirfaisal10@gmail.com>
2018-05-03 15:43:38 +0600

    plusminus

---
+++ plusminus.py
@@ -0,0 +1,16 @@
+def plusMinus(arr):
+    #
+    # Write your code here.
+    #
+    z,p,n = 0,0,0
+    for num in arr:
+        if num < 0:
+            n += 1
+        elif num == 0:
+            z += 1
+        else:
+            p += 1
+    
+    print(p/len(arr))
+    print(n/len(arr))
+    print(z/len(arr))
