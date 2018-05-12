cae4f202f5152b333ce7e73052ac0f84b97a3366
dev-tanvir <dev-tanvir@users.noreply.github.com>
2018-05-12 12:45:06 +0600

    incorrect regex solved

---
+++ Incorrect _Regex.py
@@ -0,0 +1,45 @@
+"""
+You are given a string . 
+Your task is to find out whether  is a valid regex or not.
+
+Input Format
+
+The first line contains integer , the number of test cases. 
+The next  lines contains the string .
+
+Constraints
+
+
+Output Format
+
+Print "True" or "False" for each test case without quotes.
+
+Sample Input
+
+2
+.*\+
+.*+
+Sample Output
+
+True
+False
+
+Explanation
+
+.*\+ : Valid regex. 
+.*+: Has the error multiple repeat. Hence, it is invalid.
+
+"""
+
+import re
+
+n  = int(input())
+
+for _ in range(n):
+    s = input()
+    
+    try:
+        if bool(re.compile(s)):
+            print(True)
+    except re.error:
+        print(False)
